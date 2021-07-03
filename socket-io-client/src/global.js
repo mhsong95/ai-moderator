598450b15bf76c86948074de77bec30ced035f89
seoyun.son <sallyluckyyun@gmail.com>
2021-07-03 16:07:54 +0900

    Cleanup files

--- socket-io-client/src/global.js
+++
@@ -1,9 +0,0 @@
-// global.js
-// Defines data structures that are shared across files (global).
-
-// Maps room_id to Clerk instances.
-const clerks = new Map();
-
-module.exports = {
-  clerks,
-};
