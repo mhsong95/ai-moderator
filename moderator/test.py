8b9e19d79a294e790ad83c4c58083e8fc420682f
Seoyun Son <31069340+anemoneflower@users.noreply.github.com>
2021-08-02 10:03:45 +0900

    Delete test.py

--- moderator/test.py
+++
@@ -1,9 +0,0 @@
-import subprocess
-
-def convert_and_split(filename):
-    command = ['ffmpeg', '-i', filename, '-c:a', 'pcm_f32le', 'out%09d.wav']
-    subprocess.run(command,stdout=subprocess.PIPE,stdin=subprocess.PIPE)
-
-filename = "6f3aa3b4-31db-427c-880e-d930d8a6c5f9_seoyun_1627311980035.wav"
-
-convert_and_split(filename)
